29357248199e115516a0cd47cac9a36c65418558
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-11-14 23:28:52 +0600

    Basic JS: Null VS Undefined

---
+++ L1-Basic_JavaScript/null_undefined.js
@@ -0,0 +1,15 @@
+//Null : Null is a special data type that is use as absence of an object. It makes a placeholder for value in variable that can be used after a time.
+
+//Undefined: Undefined means you didn't assign or defined any value to a variable throughout declaration.
+
+//Example: Null
+var xyz = null;
+console.log(xyz);
+
+//assign a value to this empty placeholder:
+xyz = 1250;
+console.log(xyz);
+
+//Example: Undefined
+var abc;
+console.log(abc);
