cf3b404a9257eb162a84cf5cbf234b85e57fd226
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-11-14 09:03:57 +0600

    Basic JS: Console Hello

---
+++ L1-Basic_JavaScript/hello.js
@@ -0,0 +1,12 @@
+/**
+ * 1. How to print something in console?
+ */
+
+console.log("Hello World!");
+console.log("JavaScript is the most famous language to Developer!");
+console.log("I am a JavaScript Programmer!");
+console.log("My favorite number is : "+ 10);
+console.log(9 + 9);
+console.log("9" + 9);
+console.log(95.50);
+console.log(5 * "10");
