610f1b736d8a350d67e4290fa648257c258f46a8
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-11-14 22:36:49 +0600

    Basic JS: Data Types

---
+++ L1-Basic_JavaScript/dataTypes.js
@@ -0,0 +1,6 @@
+//In JavaScript there is two type of data are available:
+// i) Primitive Data Types: This Data Types are defined by JavaScript itself;
+// ii) Object Data Types: This is user defined Data Types;
+
+//Primitive Data Types: Number, String, Boolean, Undefined, Null;
+//Object Data Types: Function, Object, Array;
