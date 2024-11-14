60f560ebfc11d22a16588c73e8e2b385806c4ce5
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-11-14 23:22:22 +0600

    Basic JS: Boolean Data Types

---
+++ L1-Basic_JavaScript/boolean.js
@@ -0,0 +1,20 @@
+/**
+ * Boolean : Boolean data types contains either true or false value. It works with conditional statement.
+ * Declaration: i) Boolean Literal, ii) Boolean Constructor;
+ */
+
+//Boolean Literal:
+var bool_1 = true;
+var bool_2 = false;
+console.log(bool_1, bool_2);
+
+//Boolean Constructor:
+var bool_3 = Boolean(false);
+var bool_4 = Boolean(true);
+console.log(bool_3, bool_4);
+
+//Conditional Boolean:
+console.log(10 == 10);
+console.log(3 < 5);
+console.log(3 > 5);
+console.log(10 != 10);
