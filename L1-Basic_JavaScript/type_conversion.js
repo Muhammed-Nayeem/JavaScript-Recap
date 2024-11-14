c625901579dcb076789493f62adfa450421ffcb5
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-11-15 00:38:15 +0600

    Basic JS: Type Conversion

---
+++ L1-Basic_JavaScript/type_conversion.js
@@ -0,0 +1,17 @@
+//Type Conversion means convert one data type into another data type by the help of Constructor Object or Built in Method:
+//Two Conversion Way:
+// i) By Constructor
+// ii) By Built In Method
+
+//By Constructor:
+var num = 1250;
+var numInStr = String(num);
+console.log(numInStr);
+
+var num_frmStr = Number(numInStr);
+console.log(num_frmStr);
+
+//By Built In Method:
+var num_2 = 1450;
+var numInStr_2 = num_2.toString();
+console.log(numInStr_2);
