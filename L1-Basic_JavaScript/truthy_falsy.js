30d3c8038233a2cbd092f0cf439ef55a48652ebe
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-11-15 00:46:36 +0600

    Basic JS: Truthy and Falsy Values

---
+++ L1-Basic_JavaScript/truthy_falsy.js
@@ -0,0 +1,13 @@
+//Truthy Values: Without 0 everything is Truthy values;
+//Falsy Values: 0, "", NAN, Null, Undefined;
+
+// console.log(1 / 0);
+
+console.log(Boolean(Infinity));
+console.log(Boolean(true));
+console.log(Boolean(-10));
+console.log(Boolean(""));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+console.log(Boolean(0));
+console.log(Boolean(undefined));
