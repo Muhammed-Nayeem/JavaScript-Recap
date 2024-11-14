3a10f525d25744296019a652c363cd7c43f55e4f
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-11-14 17:05:18 +0600

    Basic JS: Naming Convention Of Variables

---
+++ L1-Basic_JavaScript/nameConvention.js
@@ -0,0 +1,17 @@
+/**
+ * Reserved Word List: For variables naming, you can't choose the JavaScript reserved keywords or word list, like "for, loop, while, break, continue, class, forEach, reduce" etc.
+ * Meaning-Full Name: Try always declare very very meaning-full variables name;
+ * Naming-Convention: Try always CamelCase or (_) this type of things for big word of variable;
+ */
+
+//Example: reserved word
+// var class = "XII"; //Bad way -> class is a reserved or keywords of JS;
+
+//Example: meaning-full name
+var country = "Bangladesh";
+var fullName = "Muhammed Nayeem";
+
+//Example: camelCase or _
+var firstName = "Muhammed";
+var last_name = "Nayeem";
+var studentId = "1712020132";
