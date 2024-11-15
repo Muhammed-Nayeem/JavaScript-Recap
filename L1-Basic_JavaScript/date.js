e6cae62d0364ed6c0cc3b9c6d80ecd786f7f1eee
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-11-15 10:18:54 +0600

    Basic JS: Date Library Method

---
+++ L1-Basic_JavaScript/date.js
@@ -0,0 +1,16 @@
+//Date is a Js predefined method which is return a new Object of Date:
+
+var date = new Date();
+console.log(date);
+
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+console.log(date.getFullYear());
+console.log(date.getTime());
