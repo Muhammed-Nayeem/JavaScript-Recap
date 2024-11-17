7aed78b9ff3dd1918b0c76c1f0d3e10111ba2558
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-11-17 10:15:13 +0600

    Conditional JS: else if condition

---
+++ L2-Conditional_JavaScript/elseIf.js
@@ -0,0 +1,22 @@
+//Example: 01
+var a = 50;
+var b = 50;
+
+if (a > b) {
+  console.log("A is greater then B");
+} else if (a < b) {
+  console.log("B is greater then A");
+} else if (a === b) {
+  console.log("A and B is Equal");
+}
+
+//Example: 02
+var num_1 = 34;
+
+if (num_1 === 0) {
+  console.log(num_1 +" is equal to zero!");
+} else if (num_1 % 2 === 0) {
+  console.log(num_1 +" is even number!");
+} else {
+  console.log(num_1 +" is odd number!");
+}
