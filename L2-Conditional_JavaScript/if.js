37d91acfef1c8c450b474f656f86bb1c8564303f
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-11-17 09:55:47 +0600

    Conditional JS: if condition

---
+++ L2-Conditional_JavaScript/if.js
@@ -0,0 +1,18 @@
+var a = 30;
+var b = 50;
+
+if (a > b) {
+  console.log("A is greater then B");
+}
+
+if (a < b) {
+  console.log("B is greater then A");
+}
+
+if (a % 2 === 0) {
+  console.log(a +" is Even Number");
+}
+
+if (b % 2 === 1) {
+  console.log(b +" is Odd Number");
+}
